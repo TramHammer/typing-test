a4ef2f0f32273ecddbb2c3642cd2d6f05d27c661
TramHammer <78754673+TramHammer@users.noreply.github.com>
2022-05-26 13:40:59 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const w = ["the", "of", "a", "to", "you", "was", "are", "they", "have", "one", "what", "were", "there", "your", "their", "said", "do", "many", "some", "would", "other", "into", "two", "could", "been", "who", "people", "only", " find", "water", "very", "words", "where", "most", "through", "any", "another", "come", "work", "word", "does", "put", "different", "again", "old", "great", "should", "Mr", "give", "something", "thought", "both", "often", "together", "don't", "world", "want"]
+const s = ["The quick brown fox jumps over the lazy dog", ""]
 let count = 60
 let x = false
 
@@ -7,14 +8,14 @@ function generateWords(type, list, words) {
         let genWords = ""
         for (let x = 0; x < words; x++) {
             let radWord = list[Math.floor(Math.random() * list.length) + 1]
-            genWords = genWords + " " + radWord
+            genWords = genWords + radWord + " "
         }
         return genWords
     } else if (type == 1) {
         let genNum = ""
         for (let y = 0; y < words; y++) {
             let radNum = Math.floor(Math.random() * 1000) + 1
-            genNum = genNum + " " + radNum
+            genNum = genNum + radNum + " "
         }
         return genNum
     }
@@ -28,14 +29,15 @@ document.getElementById("inputText").onkeyup = function() {
     if (x == false) {
         x = true;
         var counter = setInterval(timer, 1000); //1000 will
+        async function timer() {
+            if (count <= 0) {
+              clearInterval(counter)
+              return 
+            }
+            count--
+            document.getElementById("time").innerHTML = count
+        }
     } 
 }
 
-async function timer() {
-    if (count <= 0) {
-      clearInterval(counter)
-      return 
-    }
-    count--
-    document.getElementById("time").innerHTML = count
-}
+
